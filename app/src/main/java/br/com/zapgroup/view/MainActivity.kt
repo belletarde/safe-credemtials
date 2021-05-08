cb594643ceea18f11f8624bac4cebee5817e78aa
Kevin Belletarde <kevin.belletarde@westwing.com.br>
2021-05-08 19:57:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/zapgroup/view/MainActivity.kt
@@ -0,0 +1,10 @@
+package br.com.zapgroup.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/br/com/zapgroup/view/SplashScreen.kt
@@ -0,0 +1,10 @@
+package br.com.zapgroup.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
